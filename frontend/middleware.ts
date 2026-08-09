63ab3d1220f5bb2d775379c252ce749f287e82d5
Alfie Solomons <32258713+ahmadizzman107@users.noreply.github.com>
2026-08-10 01:23:17 +0800

    Setup auth middleware

---
+++ frontend/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+  function middleware(req: NextRequestWithAuth) {
+    if (!req.nextauth.token) {
+      return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: () => true,
+    },
+  },
+);
+
+export const config = {
+  matcher: ['/users/:path*'],
+};
