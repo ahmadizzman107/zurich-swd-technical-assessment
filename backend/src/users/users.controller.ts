d200c7cb01bf4b9e34e91cb7bd5eb9ee215a4fe5
Alfie Solomons <32258713+ahmadizzman107@users.noreply.github.com>
2026-08-10 19:49:37 +0800

    Import and implement jwt guard

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService, PaginatedUsers } from './users.service';
 import { GetUsersQueryDto } from './dto/get-users-query.dto';
+import JwtAuthGuard from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -11,6 +19,7 @@ export class UsersController {
     return this.usersService.getPaginatedUsers(query.page, query.limit);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id/email')
   async getUserRealEmail(@Param('id', ParseIntPipe) id: number) {
     const email = await this.usersService.getRealEmail(id);
